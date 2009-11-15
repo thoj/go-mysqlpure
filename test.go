5fb576b54f50064de696adad02e7724494ac9dbf
tj <mail@jager.no>
2009-11-15 05:01:30 +0100

    Change test to use instance of mysql

--- test.go
+++ test.go
@@ -7,8 +7,9 @@ import (
 
 
 func main() {
-	err := mysql.Connect("127.0.0.1:3306", "test", "test", "test");
+	dbh, err := mysql.Connect("127.0.0.1:3306", "test", "test", "test");
 	if err != nil {
 		fmt.Printf("%s\n", err);
 	}
+	fmt.Printf("Connected to %s\n", dbh.ServerVersion);
 }
