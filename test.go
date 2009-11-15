6ee88ffd0580c558dac17ac62a728bca9a3a7d70
tj <mail@jager.no>
2009-11-15 21:03:24 +0100

    Simple query test. Dosen't return any data.

--- test.go
+++ test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"./mysql";
 	"fmt";
+	"os";
 )
 
 
@@ -10,11 +11,14 @@ func main() {
 	dbh, err := mysql.Connect("127.0.0.1:3306", "test", "test", "test");
 	if err != nil {
 		fmt.Printf("%s\n", err);
+		os.Exit(1);
 	}
 	fmt.Printf("Connected to %s\n", dbh.ServerVersion);
-	//var res * mysql.MySQLResponse;
-	_, err = dbh.Query("SHOW PROCESSLIST");
+	var res * mysql.MySQLResponse;
+	res, err = dbh.Query("SHOW PROCESSLIST");
 	if err != nil {
 		fmt.Printf("%s\n", err);
+		os.Exit(1);
 	}
+	fmt.Printf("%#v\n", res);
 }
