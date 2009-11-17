0e0d6032a42dc03312687027c6a0572010fb83e4
tj <mail@jager.no>
2009-11-17 01:18:28 +0100

    More complex example

--- test.go
+++ test.go
@@ -8,24 +8,27 @@ import (
 
 
 func main() {
-	dbh, err := mysql.Connect("127.0.0.1:3306", "test", "test", "test");
+	dbh, err := mysql.Connect("127.0.0.1:3306", "test", "test", "predb");
 	if err != nil {
 		fmt.Printf("%s\n", err);
 		os.Exit(1);
 	}
 	fmt.Printf("Connected to %s\n", dbh.ServerVersion);
-	_, err = dbh.Query("SHOW PROCESSLIST");
-	row := dbh.GetRow();
-	for i := 0; i < len(row.Data); i++ {	
-		fmt.Printf("%s\t", dbh.CurrentResultSet.Fields[i]);
+	_, err = dbh.Query("SELECT * FROM releases LIMIT 1000");
+
+	for row := dbh.FetchRow(); row != nil; row = dbh.FetchRow() {
+		for i := 0; i < len(row.Data); i++ {
+			fmt.Printf("%s\t", dbh.CurrentResultSet.Fields[i])
+		}
+		fmt.Printf("\n");
+		for i := 0; i < len(row.Data); i++ {
+			fmt.Printf("%s\t", row.Data[i])
+		}
+		fmt.Printf("\n");
+		if err != nil {
+			fmt.Printf("%s\n", err);
+			os.Exit(1);
+		}
 	}
-	fmt.Printf("\n");
-	for i := 0; i < len(row.Data); i++ {	
-		fmt.Printf("%s\t", row.Data[i]);
-	}
-	fmt.Printf("\n");
-	if err != nil {
-		fmt.Printf("%s\n", err);
-		os.Exit(1);
-	}
+	dbh.Quit();
 }
