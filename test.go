c6dc745910a5c5ebab2278ea960f5f71a9ec2ea9
tj <mail@jager.no>
2009-11-18 15:44:46 +0100

    New test

--- test.go
+++ test.go
@@ -8,16 +8,16 @@ import (
 
 
 func main() {
-	dbh, err := mysql.Connect("unix", "", "/var/run/mysqld/mysqld.sock", "test", "", ""); // Without password, user test, without database.
-	//dbh, err := mysql.Connect("tcp", "", "127.0.0.1:3306", "test", "test", "test"); // With password, user test, with database.
+	//dbh, err := mysql.Connect("unix", "", "/var/run/mysqld/mysqld.sock", "test", "", ""); // Without password, user test, without database.
+	dbh, err := mysql.Connect("tcp", "", "127.0.0.1:3306", "test", "test", "test");	// With password, user test, with database.
 	if err != nil {
 		fmt.Printf("%s\n", err);
 		os.Exit(1);
 	}
-	dbh.Use("test"); //Select database
+	dbh.Use("test");	//Select database
 	fmt.Printf("Connected to %s\n", dbh.ServerVersion);
 	res := new(mysql.MySQLResponse);
-//	res, err = dbh.Query("SELECT * FROM releases LIMIT 1000");
+	//	res, err = dbh.Query("SELECT * FROM releases LIMIT 1000");
 	res, err = dbh.Query("INSERT INTO test (sha1) VALUES(SHA1('foo'))");
 	fmt.Printf("%s\n", res);
 	if err != nil {
@@ -28,6 +28,22 @@ func main() {
 	fmt.Printf("%s\n", res);
 	res, err = dbh.Query("SELECT * FROM test");
 	fmt.Printf("%s\n", res);
+	for row := res.FetchRow(); row != nil; row = res.FetchRow() {
+		for i := 0; i < len(row.Data); i++ {
+			fmt.Printf("%s\t", res.ResultSet.Fields[i])
+		}
+		fmt.Printf("\n");
+		for i := 0; i < len(row.Data); i++ {
+			fmt.Printf("%s\t", row.Data[i])
+		}
+		fmt.Printf("\n");
+		if err != nil {
+			fmt.Printf("%s\n", err);
+			os.Exit(1);
+		}
+	}
+	res, err = dbh.Query("SHOW PROCESSLIST");
+	fmt.Printf("%s %s\n", res, err);
 	for row := res.FetchRow(); row != nil; row = res.FetchRow() {
 		for i := 0; i < len(row.Data); i++ {
 			fmt.Printf("%s\t", res.ResultSet.Fields[i])
