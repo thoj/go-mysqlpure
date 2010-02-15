7de9aa7f8e3b2f41c527da7fcb2c8331a3ecabcf
tj <mail@jager.no>
2010-02-15 01:24:36 +0100

    Fix tests to use FailNow

--- mysql_test.go
+++ mysql_test.go
@@ -2,76 +2,83 @@ package mysql
 
 import (
 	"testing"
-//	"testing/quick"
 )
 
 func SelectSingleRow(t *testing.T, q string) map[string]string {
-	dbh, err := Connect("tcp", "", "127.0.0.1:3306", "test", "", "test")
+	dbh, err := Connect("tcp", "", "127.0.0.1:3306", "test", "test", "test")
 	if err != nil {
 		t.Error(err)
+		t.FailNow()
 	}
 	if dbh == nil {
 		t.Error("dbh is nil")
+		t.FailNow()
 	}
 	dbh.Use("test")
-	
+
 	res, err := dbh.Query("SET NAMES utf8")
 	res, err = dbh.Query(q)
 
 	if err != nil {
 		t.Error(err)
+		t.FailNow()
 	}
 	row := res.FetchRowMap()
 	if row == nil {
 		t.Error("row is nil")
+		t.FailNow()
 	}
 	dbh.Quit()
-	return row;
+	return row
 }
 
 func SelectSingleRowPrepared(t *testing.T, q string, p ...) map[string]string {
-	dbh, err := Connect("tcp", "", "127.0.0.1:3306", "test", "", "test")
+	dbh, err := Connect("tcp", "", "127.0.0.1:3306", "test", "test", "test")
 	if err != nil {
 		t.Error(err)
+		t.FailNow()
 	}
 	if dbh == nil {
 		t.Error("dbh is nil")
+		t.FailNow()
 	}
 	dbh.Use("test")
-	
+
 	res, err := dbh.Query("SET NAMES utf8")
 	sth, err := dbh.Prepare(q)
 
 	if err != nil {
 		t.Error(err)
+		t.FailNow()
 	}
-	res,err = sth.Execute(p);
+	res, err = sth.Execute(p)
 	row := res.FetchRowMap()
 	if row == nil {
 		t.Error("row is nil")
+		t.FailNow()
 	}
 	dbh.Quit()
-	return row;
+	return row
 }
 
 func TestSelectString(t *testing.T) {
-	row := SelectSingleRow(t, "SELECT * FROM test WHERE name='test1'");
-	test := "1234567890abcdef";
+	row := SelectSingleRow(t, "SELECT * FROM test WHERE name='test1'")
+	test := "1234567890abcdef"
 	if row["stuff"] != test {
 		t.Error(row["stuff"], " != ", test)
 	}
 }
 
 func TestSelectStringPrepared(t *testing.T) {
-	row := SelectSingleRowPrepared(t, "SELECT * FROM test WHERE name=?", "test1");
-	test := "1234567890abcdef";
+	row := SelectSingleRowPrepared(t, "SELECT * FROM test WHERE name=?", "test1")
+	test := "1234567890abcdef"
 	if row["stuff"] != test {
 		t.Error(row["stuff"], " != ", test)
 	}
 }
 
 func TestSelectUFT8(t *testing.T) {
-	row := SelectSingleRow(t, "SELECT * FROM test WHERE name='unicodetest1'");
+	row := SelectSingleRow(t, "SELECT * FROM test WHERE name='unicodetest1'")
 	test := "l̡̡̡ ̴̡ı̴̴̡ ̡̡͡|̲̲̲͡͡͡ ̲▫̲͡ ̲̲̲͡͡π̲̲͡͡ ̲̲͡▫̲̲͡͡ ̲|̡̡̡ ̡ ̴̡ı̴̡̡ ̡"
 	if row["stuff"] != test {
 		t.Error(row["stuff"], " != ", test)
@@ -79,7 +86,7 @@ func TestSelectUFT8(t *testing.T) {
 }
 
 func TestSelectUFT8Prepared(t *testing.T) {
-	row := SelectSingleRowPrepared(t, "SELECT * FROM test WHERE name=?", "unicodetest1");
+	row := SelectSingleRowPrepared(t, "SELECT * FROM test WHERE name=?", "unicodetest1")
 	test := "l̡̡̡ ̴̡ı̴̴̡ ̡̡͡|̲̲̲͡͡͡ ̲▫̲͡ ̲̲̲͡͡π̲̲͡͡ ̲̲͡▫̲̲͡͡ ̲|̡̡̡ ̡ ̴̡ı̴̡̡ ̡"
 	if row["stuff"] != test {
 		t.Error(row["stuff"], " != ", test)
